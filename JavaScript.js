2c1abcc1b559bbd93693b69d08b6d164641f0e68
SAMIYAPPAN M <samisamy4597m@gmail.com>
2025-03-25 13:27:57 +0530

    SAM

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+﻿const text = "Welcome to My Portfolio";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing-effect").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+window.onload = typeEffect;
